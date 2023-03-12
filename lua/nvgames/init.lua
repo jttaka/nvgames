8eb25575eda7baaa2b62cae6e488452d8cf73612
John Takala <john.t.takala@gmail.com>
2023-03-12 05:08:51 -0800

    Create NVGames command

--- lua/nvgames/init.lua
+++ lua/nvgames/init.lua
@@ -1,7 +1,11 @@
 local function printer()
-	print("hello")
+	print("Hello")
 end
-
+local function nv_command ()
+	printer()
+end
+vim.api.nvim_create_user_command("NVGames", nv_command, {})
 return {
 	printer = printer
+
 }
